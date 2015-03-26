b5b8224afe8236e51eb59dabcbe8c481098f1e35
Augusto Destrero <augu@aruba.it>
2015-03-26 11:51:26 +0100

    added download_url

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email = 'a.destrero@gmail.com',
     description = 'Utility functions to deal with working days in date operations. With builtin localization support.',
     url = 'https://github.com/baxeico/pyworkingdays',
+    download_url = 'https://github.com/baxeico/pyworkingdays/archive/0.1.tar.gz',
     install_requires = ['python-dateutil'],
     keywords = ['date', 'business', 'working']
 )
